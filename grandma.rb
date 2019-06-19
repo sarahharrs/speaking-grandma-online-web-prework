ffec8bed5a0ab3f69d18fafbfa640d8c075c34f6
Sarah Harrs <sarahharrs@gmail.com>
2019-06-19 23:24:03 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,4 @@
 phrase = " I LOVE YOU GRANDMA!"
-end
 def speak_to_grandma(phrase)
   if phrase=="I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
@@ -8,4 +7,4 @@ puts "NO, NOT SINCE 1938!"
 else 
   puts "HUH?! SPEAK UP,SONNY!"
 end
-
+end
